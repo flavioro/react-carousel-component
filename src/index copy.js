49de7c40933c5d7173c9bb2c23c3b852a357ec6d
flavioro <flavioro@gmail.com>
2020-12-07 14:36:27 -0300

    Changed set carousel

--- src/index copy.js
+++ src/index copy.js
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Carousel from "react-elastic-carousel";
-
 import Item from "./Item";
-import Slide from './slide'
-
 import "./styles.css";
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
   { width: 550, itemsToShow: 2 },
   { width: 768, itemsToShow: 3 },
-  // { width: 1200, itemsToShow: 4 },
+  { width: 1200, itemsToShow: 4 },
 ];
 
 function App() {
@@ -20,7 +17,7 @@ function App() {
       <h1 style={{ textAlign: "center" }}>Example to setup your carousel in react</h1>
       <div className="App">
         <Carousel breakPoints={breakPoints}>
-          <Item>One<Slide /></Item>
+          <Item>One</Item>
           <Item>Two</Item>
           <Item>Three</Item>
           <Item>Four</Item>
---
+++ src/slide.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import photo from './img/casa_europa_archshop.jpg'
+
+export default function Slide() {
+
+  return (
+    <>
+      <a href="planta-pronta/casa-europa-104a.html">
+        <img style={{width:"297px"}}
+          // 
+          // height="168px"
+          // loading="lazy"
+          // className="img-fluid mx-auto d-block"
+          src={photo}
+          alt="Casa Europa"
+        />
+      </a>
+  </>
+  )
+}
